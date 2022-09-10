65cd117b3cf3d5442826d8e9f4c93fa067912a07
angie <angheloalf95@gmail.com>
2022-09-10 12:11:46 -0400

    Reimplement settimes and dotime

--- src/5.3/cc.c
+++ src/5.3/cc.c
@@ -1,19 +1,17 @@
+#define _POSIX_C_SOURCE 199309L
+
 #include "header.h"
 
+#include <unistd.h>
 #include <sys/times.h>
-
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <time.h> 
-#include <sys/times.h> 
-
+#include <sys/resource.h>
+#include <time.h>
 
 typedef int32_t s32;
 typedef uint32_t u32;
 typedef double f64;
 
+#ifdef ORIGINAL_BEHAVIOUR
 // 0x10009c18
 struct tms D__0x7A28 = { 0 };
 // 0x10009c10
@@ -64,3 +62,73 @@ void f_dotime(uint8_t *mem, uint32_t sp) {
 
     fprintf(stderr, "%.2fu %.2fs %u:%04.1f %.0f%%\n", sp58, sp50, (u32) (sp68 / (60 * clock_ticks)), (sp68 % (u32)(60 * clock_ticks)) / clock_ticks, (((sp58 + sp50) / (sp68 / clock_ticks)) * 100.0));
 }
+#else
+struct timespec sWallTimePrev = { 0 };
+struct rusage sRUsageSelfPrev = { 0 };
+struct rusage sRUsageChildrenPrev = { 0 };
+
+void f_settimes(uint8_t *mem, uint32_t sp) {
+    int retWall;
+    int retSelf;
+    int retChildren;
+
+    retWall = clock_gettime(CLOCK_MONOTONIC, &sWallTimePrev);
+    if (retWall == -1) {
+        perror("clock_gettime(CLOCK_MONOTONIC, &sWallTimePrev)");
+    }
+
+    retSelf = getrusage(RUSAGE_SELF, &sRUsageSelfPrev);
+    if (retSelf == -1) {
+        perror("getrusage(RUSAGE_SELF, &sRUsageSelfPrev)");
+    }
+
+    retChildren = getrusage(RUSAGE_CHILDREN, &sRUsageChildrenPrev);
+    if (retChildren == -1) {
+        perror("getrusage(RUSAGE_CHILDREN, &sRUsageChildrenPrev)");
+    }
+
+    assert(retSelf != -1);
+    assert(retChildren != -1);
+}
+
+void f_dotime(uint8_t *mem, uint32_t sp) {
+    int retWall;
+    int retSelf;
+    int retChildren;
+    struct timespec wallTimeSpec;
+    struct rusage usageSelf = { 0 };
+    struct rusage usageChildren = { 0 };
+
+    retWall = clock_gettime(CLOCK_MONOTONIC, &wallTimeSpec);
+    if (retWall == -1) {
+        perror("clock_gettime(CLOCK_MONOTONIC, &wallTimeSpec)");
+    }
+
+    retSelf = getrusage(RUSAGE_SELF, &usageSelf);
+    if (retSelf == -1) {
+        perror("getrusage(RUSAGE_SELF, &usageSelf)");
+    }
+
+    retChildren = getrusage(RUSAGE_CHILDREN, &usageChildren);
+    if (retChildren == -1) {
+        perror("getrusage(RUSAGE_CHILDREN, &usageChildren)");
+    }
+
+    assert(retSelf != -1);
+    assert(retChildren != -1);
+
+    double wallTimeDiff;
+    double userMiliseconds;
+    double systemMiliseconds;
+
+#define GET_WALL_AS_MILIS(spec) (((spec)->tv_sec * 1000.0L) + ((spec)->tv_nsec / (1000.0L * 1000.0L)))
+    wallTimeDiff = GET_WALL_AS_MILIS(&wallTimeSpec) - GET_WALL_AS_MILIS(&sWallTimePrev);
+
+#define GET_UTIME(timevalptr) (((timevalptr)->tv_sec * 1000.0L * 1000.0L) + (timevalptr)->tv_usec)
+
+    userMiliseconds = ((GET_UTIME(&usageSelf.ru_utime) + GET_UTIME(&usageChildren.ru_utime)) - (GET_UTIME(&sRUsageSelfPrev.ru_utime) + GET_UTIME(&sRUsageChildrenPrev.ru_utime))) / 1000.0L;
+    systemMiliseconds = ((GET_UTIME(&usageSelf.ru_stime) + GET_UTIME(&usageChildren.ru_stime)) - (GET_UTIME(&sRUsageSelfPrev.ru_stime) + GET_UTIME(&sRUsageChildrenPrev.ru_stime))) / 1000.0L;
+
+    fprintf(stderr, "%.2fu %.2fs %.2f %.0f%%\n", userMiliseconds, systemMiliseconds, wallTimeDiff, (((userMiliseconds + systemMiliseconds) / wallTimeDiff) * 100));
+}
+#endif
