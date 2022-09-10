45ec6b7abb2115e893832b55016cbe2c09240a2d
angie <angheloalf95@gmail.com>
2022-09-10 09:59:22 -0400

    cleanup the code a bit

--- src/5.3/src_cc.c
+++ src/5.3/src_cc.c
@@ -6,36 +6,23 @@ typedef int32_t s32;
 typedef double f64;
 
 // 0x10009c18
-//struct tms_irix D__0x7A28 = { 0 };
 struct tms D__0x7A28 = { 0 };
 // 0x10009c10
 clock_t D__0x7A2C = 0;
-
-//extern ? D__0x7F70;
-//extern ? D__0x7FA8;
-
-// extern s32 D__0x7A2C;
 
 void f_settimes(uint8_t *mem, uint32_t sp) {
     D__0x7A2C = times(&D__0x7A28);
 }
 
 void f_dotime(uint8_t *mem, uint32_t sp) {
-    s32 sp6C;
     s32 sp68;
     f64 sp58;
     f64 sp50;
-    struct tms_irix sp40;
-    f64 temp_f10;
-    f64 temp_f6;
+    struct tms sp40;
 
-    sp6C = times(&sp40);
-    sp68 = sp6C - D__0x7A2C;
-    temp_f10 = (f64) (((sp40.tms_utime + sp40.tms_cutime) - D__0x7A28.tms_utime) - D__0x7A28.tms_cutime) / 100.0;
-    temp_f6 = (f64) (((sp40.tms_stime + sp40.tms_cstime) - D__0x7A28.tms_stime) - D__0x7A28.tms_cstime) / 100.0;
-    sp58 = temp_f10;
-    sp50 = temp_f6;
-    //fprintf(&D__0x7F70 + 0x20, &D__0x7FA8 + 0x66E4, second half of f64, temp_f10, temp_f6, sp68 / 6000, (f64) (sp68 % 6000) / 100.0, second half of f64, (bitwise f32) (((temp_f10 + temp_f6) / ((f64) sp68 / 100.0)) * 100.0));
+    sp68 = times(&sp40) - D__0x7A2C;
+    sp58 = (((sp40.tms_utime + sp40.tms_cutime) - D__0x7A28.tms_utime) - D__0x7A28.tms_cutime) / 100.0;
+    sp50 = (((sp40.tms_stime + sp40.tms_cstime) - D__0x7A28.tms_stime) - D__0x7A28.tms_cstime) / 100.0;
 
-    fprintf(stderr, "%.2fu %.2fs %u:%04.1f %.0f%%\n", temp_f10, temp_f6, sp68 / 6000, (f64) (sp68 % 6000) / 100.0,(((temp_f10 + temp_f6) / ((f64) sp68 / 100.0)) * 100.0));
+    fprintf(stderr, "%.2fu %.2fs %u:%04.1f %.0f%%\n", sp58, sp50, sp68 / 6000, (sp68 % 6000) / 100.0, (((sp58 + sp50) / (sp68 / 100.0)) * 100.0));
 }
